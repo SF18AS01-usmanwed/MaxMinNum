bf9928a1089bc0d496a4d210423d6d9d128131d1
Ousmane Ouedraogo <wkouedraogo48@yahoo.com>
2019-01-08 13:32:04 -0500

    Commit.

--- MaxMinNum/ViewController.swift
+++ MaxMinNum/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .yellow
+        
+        let bounds = maxMin( num: [ 8,3,-4 ,98,7,10,105])
+        label.text = String (" min is \(bounds.min) and max is \(bounds.max)")
+
     }
+    
 
-
+    @IBAction func returnButton(_ sender: UITextField) {
+       sender.resignFirstResponder()
+    }
 }
+
+
+func maxMin(num: [Int]) -> (min: Int, max: Int) {
+    var currMin = num[0]
+    var currMax = num[0]
+    for value in num[1..<num.count] {
+        if value < currMin {
+            currMin = value
+        }
+        else if value > currMax {
+            currMax = value
+        }
+    }
+    return ( currMin, currMax )
+    
+}
+
 
